0c7a4945e3b59ae5570e86453be7301320b584de
Hamza Naveed Malik <hamza.malik@kindredgroup.com>
2026-05-18 00:08:46 +0500

    Siri-Glow

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -55,8 +55,8 @@ function CommandDialog({
         )}
         showCloseButton={showCloseButton}
       >
-        <div className="search-modal-glow search-glow-wrapper glow-active w-full">
-          <div className="search-modal-glow-inner shadow-lg">
+        <div className="search-modal-glow w-full">
+          <div className="search-modal-inner">
             <Command className="rounded-none bg-transparent [&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
               {children}
             </Command>
